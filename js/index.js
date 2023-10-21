5fd7e12ad4b6c7e173ab16bf6148f60da7f873d3
Diego Gilardoni <dgilardoni5@gmail.com>
2023-10-21 02:47:21 -0300

    cambios en interfaz y js para mostrar y ocultar

--- js/index.js
+++ js/index.js
@@ -151,12 +151,18 @@ document.getElementById('statsButton').addEventListener('click', () => {
       });
 });
 
+//Mostrar ocultar - collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-const pokedexTitles = document.querySelectorAll('.pokedex-title');
-
-pokedexTitles.forEach(title => {
-    title.addEventListener('click', () => {
-        const content = title.nextElementSibling;
-        content.style.display = content.style.display === 'none' ? 'block' : 'none';
-    });
-});
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
